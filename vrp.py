f6748269720b80169354791361b7750fbf9aa231
Chia Jin Jun <144515009+jinjunchia@users.noreply.github.com>
2025-03-05 15:15:44 +0800

    Added project files

---
+++ vrp.py
@@ -0,0 +1,53 @@
+import sys
+import itertools
+
+"""
+To use this file with example testcases, run: 
+
+python vrp.py < 1.in > 1.out
+
+This reads input from 1.in and prints output to 1.out. 
+"""
+
+def read_input():
+    """Reads input from stdin and returns number of locations, vehicle capacity, distance matrix, and demand vector."""
+    n = int(sys.stdin.readline().strip())
+    Q = int(sys.stdin.readline().strip())
+    
+    D = []
+    for _ in range(n):
+        D.append(list(map(int, sys.stdin.readline().strip().split())))
+    
+    q = list(map(int, sys.stdin.readline().strip().split()))
+    
+    return n, Q, D, q
+
+def solve_cvrp(n, Q, D, q):
+    """TODO: Solve the Capacitated Vehicle Routing Problem and return a list of routes."""
+    routes = [[0]]
+    
+    return routes
+
+def check(routes, n, Q, D, q):
+    node_visited = []
+    for route in routes:
+        total_demand = sum([q[i] for i in route if i != 0])
+        if not (total_demand <= Q):
+            return False
+        node_visited += route
+
+    if len(set(node_visited)) != len(q):
+        return False
+
+    return True
+
+def main():
+    n, Q, D, q = read_input()
+    routes = solve_cvrp(n, Q, D, q)
+
+    if check(routes, n, Q, D, q): 
+        for route in routes:
+            print(" ".join(map(str, route)))
+
+if __name__ == "__main__":
+    main()
